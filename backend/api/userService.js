116f520e4587469c73fae4eb633e55fb8e52721f
oscargregorius <oscar.gregorius@gmail.com>
2022-01-11 13:46:52 +0100

    register done ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

--- backend/api/userService.js
+++ backend/api/userService.js
@@ -1,21 +1,31 @@
 const crypto = require("crypto");
+const userModel = require("../models/user");
+const roles = require("../models/role");
 
 const secret = "goodLuckToHackThisSaltMsgBrevet00@haha.se";
 
 module.exports = user = (app) => {
   //login
-  app.post("/api/login", (req, res) => {
-    //import model
-
-    // see if user exists
-
-    //encrypt password
+  app.post("/api/login", async (req, res) => {
+    if (req.session?.user) {
+      res.sendStatus(400);
+      return;
+    }
     const hash = crypto
       .createHmac("sha256", secret)
-      .update(req.body.password)
+      .update(req.body?.password)
       .digest("hex");
 
-    //send response, save
+    let user = await userModel.findOne({
+      email: req.body.email,
+      password: hash,
+    });
+    if (user) {
+      req.session.user = user;
+      res.sendStatus(200);
+      return;
+    }
+    res.sendStatus(400);
   });
 
   //logout
@@ -23,25 +33,43 @@ module.exports = user = (app) => {
     if (req.session?.user) {
       delete req.session.user;
       res.sendStatus(200);
-    } else {
-      res.sendStatus(400);
+      return;
     }
+    res.sendStatus(400);
   });
 
   //register
-  app.post("/api/register", (req, res) => {
-    // fetch user model
+  app.post("/api/register", async (req, res) => {
+    let userAlreadyExistsByEmail = await userModel.findOne({
+      email: req.body?.email,
+    });
+    let userAlreadyExistsByUsername = await userModel.findOne({
+      username: req.body?.username,
+    });
 
-    //validate email if it already exists
+    if (userAlreadyExistsByEmail || userAlreadyExistsByUsername) {
+      res.sendStatus(400);
+      return;
+    }
 
     const hash = crypto
       .createHmac("sha256", secret)
-      .update(req.body.password)
+      .update(req.body?.password)
       .digest("hex");
 
-    // create user
-
-    //save and send response
+    try {
+      let user = new userModel({
+        ...req.body,
+        password: hash,
+        roles: [roles.USER],
+      });
+      await user.save();
+      res.sendStatus(200);
+      return;
+    } catch (error) {
+      res.sendStatus(400);
+      return;
+    }
   });
 
   //current user
--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,12 @@ global.mongoose = require("mongoose");
 const express = require("express");
 const user = require("./api/userService");
 const banlist = require("./api/banService");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const url = process.env.URL;
 user(app);
@@ -18,8 +22,8 @@ global.mongoose
     console.log("connected to mongoose");
   });
 
-const floodControl = require('./middlewares/floodControl.js')
-app.use(floodControl)
+const floodControl = require("./middlewares/floodControl.js");
+app.use(floodControl);
 
 app.listen(process.env.PORT, () => {
   console.log("app started: ", process.env.PORT);
