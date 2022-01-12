6e4ccb613b4a5e988f6c5d5491268e7387da9134
postila <isabella.postila@hotmail.com>
2022-01-12 11:51:15 +0100

    refactoring
    Co-Authored-By: bextex <72066237+bextex@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>

--- backend/api/userService.js
+++ backend/api/userService.js
@@ -1,12 +1,12 @@
 const crypto = require("crypto");
 const userModel = require("../models/user");
 const roles = require("../models/role");
-const banModel = require("../models/ban");
-const { authUserLoggedIn, authUserNotLoggedIn, bannedUser, authRole } = require("../middlewares/validation")
+const { notBannedUser, userNotExists } = require("../middlewares/validation")
+const { authUserLoggedIn, authUserNotLoggedIn, authRole } = require("../middlewares/acl")
 
 module.exports = user = (app) => {
   //login
-  app.post("/api/login", authUserNotLoggedIn, bannedUser, async (req, res) => {
+  app.post("/api/login", authUserNotLoggedIn, notBannedUser, async (req, res) => {
     const hash = crypto
       .createHmac("sha256", process.env.SECRET)
       .update(req.body?.password)
@@ -36,24 +36,7 @@ module.exports = user = (app) => {
   });
 
   //register
-  app.post("/api/register", async (req, res) => {
-    let userAlreadyExistsByEmail = await userModel.findOne({
-      email: req.body?.email,
-    });
-    let userAlreadyExistsByUsername = await userModel.findOne({
-      username: req.body?.username,
-    });
-    let userIsBanned = await banModel.findOne({ email: req.body?.email });
-
-    if (
-      userAlreadyExistsByEmail ||
-      userAlreadyExistsByUsername ||
-      userIsBanned
-    ) {
-      res.sendStatus(403);
-      return;
-    }
-
+  app.post("/api/register", authUserNotLoggedIn, notBannedUser, userNotExists, async (req, res) => {
     const hash = crypto
       .createHmac("sha256", process.env.SECRET)
       .update(req.body?.password)
