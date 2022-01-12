3ec1719bc4441dc78dc4aab2dfc4611abae80262
postila <isabella.postila@hotmail.com>
2022-01-12 11:51:03 +0100

    refactored and added check if user already exists
    Co-Authored-By: bextex <72066237+bextex@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -1,24 +1,7 @@
 const banModel = require("../models/ban");
+const userModel = require("../models/user");
 
-function authUserNotLoggedIn(req, res, next) {
-  if (req.session?.user) {
-    res.sendStatus(400);
-    return;
-  }
-  
-  next()
-}
-
-function authUserLoggedIn(req, res, next) {
-  if (!req.session?.user) {
-    res.sendStatus(401);
-    return;
-  }
-
-  next()
-}
-
-async function bannedUser(req, res, next) {
+async function notBannedUser(req, res, next) {
   if (!req.body.email || !req.body.password) {
     res.sendStatus(400);
     return;
@@ -33,26 +16,28 @@ async function bannedUser(req, res, next) {
   next()
 }
 
-function authRole(roles) {
-  return (req, res, next) => {
-
-    let found = false
-    for (let foundRole of req.session.user.roles) {
-      if (roles.includes(foundRole)) {
-        found = true
-      }
-    }
-    
-    if (!found) {
-      return res.sendStatus(401);
-    }
-    next()
+async function userNotExists(req, res, next) {
+  if (!req.body.email || !req.body.username) {
+    res.sendStatus(400);
+    return;
   }
+
+  let userAlreadyExistsByEmail = await userModel.findOne({
+      email: req.body?.email,
+    });
+  let userAlreadyExistsByUsername = await userModel.findOne({
+      username: req.body?.username,
+    });
+
+  if (userAlreadyExistsByEmail || userAlreadyExistsByUsername) {
+    res.sendStatus(400);
+    return;
+  }
+
+  next()
 }
 
 module.exports = {
-  authUserNotLoggedIn,
-  authUserLoggedIn,
-  bannedUser,
-  authRole
+  notBannedUser,
+  userNotExists,
 }
