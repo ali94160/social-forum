000398104637c29b0659566ee98c589ceb347960
postila <isabella.postila@hotmail.com>
2022-01-12 11:58:06 +0100

    added post in app
    Co-Authored-By: bextex <72066237+bextex@users.noreply.github.com>
    Co-Authored-By: Alexander S. <70532114+autumnjava@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ global.mongoose = require("mongoose");
 const express = require("express");
 const user = require("./api/userService");
 const banlist = require("./api/banService");
+const post = require("./api/postService");
 const session = require("express-session");
 require("dotenv").config();
 
@@ -21,6 +22,7 @@ app.use(
 const url = process.env.URL;
 user(app);
 banlist(app);
+post(app);
 
 global.mongoose
   .connect(url, {
