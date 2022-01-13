800e98348bc15ac2181307ff939792fec1b93e92
autumnjava <creizy@gmail.com>
2022-01-13 16:29:24 +0100

    logic for find and later delete user

--- backend/api/userService.js
+++ backend/api/userService.js
@@ -9,5 +9,43 @@ const {
 } = require("../middlewares/acl");
 
 module.exports = user = (app) => {
+  // delete your self
+  app.delete(
+    "/api/user/self",
+    authUserLoggedIn,
+    authRole([roles.USER]),
+    async (req, res) => {
+      const user = { ...req.session.user };
+      const password = req.body.password
+
+      if(!password) {
+        res.json('where is password?');
+        // res.sendStatus(400); // bad request
+        return;
+      }
+
+      const hash = crypto
+        .createHmac("sha256", process.env.SECRET)
+        .update(password)
+        .digest("hex");
+
+        if(user.password !== hash){
+          res.json('password not matchy match');
+          // res.sendStatus(400);
+          return;
+        }
+        
+        // if we made it all the way here all good, deleting user!
+        try{
+          // const userFromDb = await userModel.findByIdAndDelete(user._id);
+          const userFromDb = await userModel.findById(user._id).exec();
+          // res.sendStatus(200);
+          res.json('deleting user from db');
+        } catch(err){
+          res.json('smth went really wrong oj');
+          return;
+        }
+    }
+  );
 
 };
