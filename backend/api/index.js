9b5362afb1718ec3670a1e7e480c3be0f54a49fc
autumnjava <creizy@gmail.com>
2022-01-14 10:22:58 +0100

    import commentService

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,10 +2,12 @@ const user = require("./userService");
 const auth = require("./authService");
 const banlist = require("./banService");
 const post = require("./postService");
+const comment = require("./commentService");
 
 module.exports = (app) => {
 user(app);
 auth(app);
 banlist(app);
 post(app);
+comment(app);
 }
