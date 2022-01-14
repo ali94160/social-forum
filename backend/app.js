a58fe17009140776d8fb53531730f8b00ba21c6e
bextex <r.sarvegard@gmail.com>
2022-01-14 15:47:28 +0100

    added to only listen to port and get console log if NOT in test env

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,12 @@ const session = require("express-session");
 const { startSession } = require('mongoose');
 const allApis = require('./api');
 const floodControl = require("./middlewares/floodControl.js");
-
 require("dotenv").config();
+const url = process.env.URL;
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const url = process.env.URL;
 
 app.use(
   session({
@@ -23,24 +22,26 @@ app.use(
 
 allApis(app);
 
-
 global.mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
-    if (global.mongoose.connection.readyState === 1 && process.env.NODE_ENV !== 'test') {
-    console.log('Db is connected!')
-  }
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then(() => {
+      if (global.mongoose.connection.readyState === 1 && process.env.NODE_ENV !== 'test') {
+      console.log('Db is connected!')
+    }
   })
 
 app.use(floodControl);
 
 // so test doesnt use this port
 if (process.env.NODE_ENV !== 'test') {
+
   app.listen(process.env.PORT, () => {
     console.log("app started: ", process.env.PORT);
   });
 }
 
 module.exports = app;
+
+
