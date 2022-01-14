a43766c711ba79a9746d676a516f623beed82eb1
oscargregorius <oscar.gregorius@gmail.com>
2022-01-14 14:03:34 +0100

    postCard comp with mockdata ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

---
+++ frontend/src/components/postCard/PostCard.tsx
@@ -0,0 +1,15 @@
+// will be removed
+const mockData = {
+  title: "Funny meme",
+  content: "hehe this is a funny meme hahahaha",
+  commentLength: 4,
+  user: {
+    username: "kalle anka",
+  },
+};
+
+function PostCard() {
+  return <div></div>;
+}
+
+export default PostCard;
