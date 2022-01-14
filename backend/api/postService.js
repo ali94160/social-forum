f187e6f119430af2ef7d90c91e232986ec5d3d48
Qiling <professorqtaku@gmail.com>
2022-01-14 13:08:35 +0100

    add sort

--- backend/api/postService.js
+++ backend/api/postService.js
@@ -2,7 +2,8 @@ const postModel = require("../models/post")
 
 module.exports = function (app) {
   app.get("/api/posts", async (req, res) => {
-    let posts = await postModel.find({});
+    let sort = req.body.sort;
+    let posts = await postModel.find({}).sort(sort);
     if (posts.length > 0) {
       res.status(200).json(posts)
     }
@@ -11,5 +12,5 @@ module.exports = function (app) {
     }
   })
 
-  
+
 }
