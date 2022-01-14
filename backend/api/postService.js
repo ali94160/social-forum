e80e13ecaaf63f8f49acee512f67dd7cede040ad
Qiling <professorqtaku@gmail.com>
2022-01-14 10:27:14 +0100

    create get all post backend

--- backend/api/postService.js
+++ backend/api/postService.js
@@ -1,3 +1,15 @@
-module.exports = function (app) {
+const postModel = require("../models/post")
 
+module.exports = function (app) {
+  app.get("/api/posts", async (req, res) => {
+    let posts = await postModel.find({});
+    if (posts.length > 0) {
+      res.status(200).json(posts)
+    }
+    else {
+      res.sendStatus(204)
+    }
+  })
+
+  
 }
