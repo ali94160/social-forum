40cebba2d60532c69a7377698da2a3efad23395f
bextex <r.sarvegard@gmail.com>
2022-01-17 14:29:13 +0100

    added method to get post from page

--- frontend/src/pages/post/PostDetailPage.tsx
+++ frontend/src/pages/post/PostDetailPage.tsx
@@ -1,9 +1,34 @@
 import Post from '../../components/post/Post';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { usePost } from '../../context/PostContext';
 
 function PostDetailPage() {
+  const id = useParams();
+  const { getPost } = usePost();
+  const [status, setStatus] = useState(0);
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    handlePost();
+  }, []);
+
+  const handlePost = async () => {
+    const res = await getPost(id);
+    console.log('post', res.body)
+    setStatus(res.status);
+    setPost(res.body);
+  }
+
+  if (status === 0 || status === 404) {
+    
+  }
+
+
+
   return (
     <div>
-      <Post />
+      <Post id={id} post={post} />
       
     </div>
   )
