03716f03ae529ff568550f60bf667e6395f1982f
Qiling <professorqtaku@gmail.com>
2022-01-17 08:44:14 +0100

    remove tests for post

--- backend/api/post/__test__/post.test.js
+++ backend/api/post/__test__/post.test.js
@@ -5,14 +5,4 @@ const request = supertest(app);
 
 describe("test post APIs", () => {
 
-  test("GET /api/posts", async () => {
-    // const res = await request.get("/api/posts");
-    // expect(res.length).toBeGreaterThan(0);
-  });
-  test("GET /api/posts with sort by title then createdDate", async () => {
-    // const res = await request.get("/api/posts");
-    // expect(res.length).toBeGreaterThan(0);
-  });
-
-
 });
