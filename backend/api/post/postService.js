21e3366bd880c15f98190e901bf352cd7749cd2d
oscargregorius <oscar.gregorius@gmail.com>
2022-01-17 10:24:09 +0100

    hotfix ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com> ^
    Co-authored-by: ProfessorQtaku <professorQtaku@gmail.com>

--- backend/api/post/postService.js
+++ backend/api/post/postService.js
@@ -1,26 +1,55 @@
 const postModel = require("../../models/post");
-const commentModel = require("../../models/comment")
+const commentModel = require("../../models/comment");
+const { authUserLoggedIn } = require("../../middlewares/acl");
 
 module.exports = function (app) {
   app.get("/api/posts", async (req, res) => {
     let posts = [];
     try {
-      let data = await postModel.find({}).sort(req.query).lean().populate("ownerId", "username");
+      let data = await postModel
+        .find({})
+        .sort(req.query)
+        .lean()
+        .populate("ownerId", "username");
       for (let post of data) {
         let commentLength = await commentModel
           .find({ postId: post._id })
           .count();
         let resPost = { ...post, commentLength };
-        posts.push(resPost)
+        posts.push(resPost);
       }
     } catch (e) {
       res.sendStatus(400);
-      return
+      return;
     }
     if (posts.length > 0) {
       res.status(200).json(posts);
     } else {
       res.sendStatus(204);
+    }
+  });
+
+  app.post("/api/user/posts", authUserLoggedIn, async (req, res) => {
+    if (!req.body) {
+      res.sendStatus(403);
+      return;
+    }
+    try {
+      let newPost = new postModel({
+        ...req.body,
+        createdDate: Date.now(),
+        ownerId: req.session.user._id,
+      });
+      const result = await newPost.save();
+      if (!result) {
+        res.sendStatus(400);
+        return;
+      }
+      res.sendStatus(200);
+      return;
+    } catch (error) {
+      res.sendStatus(400);
+      return;
     }
   });
 };
