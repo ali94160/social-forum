51de226a10107d8b373f17a58b32fe3080d11600
Qiling <professorqtaku@gmail.com>
2022-01-17 13:58:06 +0100

    hotfix replace !important to &&{}

--- frontend/src/components/auth-modal/login-form/StyledLoginForm.ts
+++ frontend/src/components/auth-modal/login-form/StyledLoginForm.ts
@@ -7,13 +7,17 @@ export const StyledInputContainer = styled(Box)`
 `;
 
 export const StyledCloseButton = styled(Button)`
-  background: grey !important;
-  font-weight: bold !important;
+&&{
+  background: grey;
+  font-weight: bold;
+}
 `;
 
 export const StyledRegisterButton = styled(Button)`
-  text-transform: none !important;
-  color: var(--dark-teal) !important;
+&&{
+  text-transform: none;
+  color: var(--dark-teal);
+}
 `;
 
 export const StyledForm = styled.form`
