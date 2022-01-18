e5f07fe5db9c31fb13a1744a5c492fd153b74927
postila <isabella.postila@hotmail.com>
2022-01-18 14:07:20 +0100

    added a try/catch to handle if id does not exists

--- backend/middlewares/postOwner.js
+++ backend/middlewares/postOwner.js
@@ -1,11 +1,15 @@
 const postModel = require("../models/post");
 
 async function isPostOwner(req, res, next) {
-  post = await postModel.findOne({ _id: req.params.id }).lean();
-  if (post.ownerId._id != req.session.user._id) {
-    return res.sendStatus(401);
+  try {
+    const post = await postModel.findOne({ _id: req.params.id }).lean().exec();
+    if (post.ownerId._id != req.session.user._id) {
+      return res.sendStatus(401);
+    }
+    next()
+  } catch (error) {
+    res.sendStatus(404)
   }
-  next()
 }
 
 module.exports = {
