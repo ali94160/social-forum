a8066a67dfaa7a42371132e0d0449838790eb059
bextex <r.sarvegard@gmail.com>
2022-01-18 11:18:18 +0100

    added data to post card

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,8 +1,10 @@
-import { StyledPost, StyledGrid } from './StyledPost';
+import { StyledPost, StyledGrid, StyledLeftGrid, StyledBottomGrid } from './StyledPost';
 import Avatar from '../avatar/Avatar';
 import Grid from '@mui/material/Grid';
 import EditIcon from '@mui/icons-material/Edit';
-import { PostEx } from '../../interfaces/Post';
+import { useEffect, useState } from 'react';
+import { User} from '../../interfaces/User';
+import { StyledAvatar } from '../postCard/StyledPostCard'
 
 interface Props {
   id: any;
@@ -10,6 +12,35 @@ interface Props {
 }
 
 function Post({ id, post }: Props) {
+  const date = post.createdDate?.substr(0, 10);
+  const time = new Date(post.createdDate);
+  const [moderators, setModerators] = useState('');
+
+  useEffect(() => {
+    handleModerators()
+  }, [post.moderatorsIds])
+
+  const handleModerators = () => {
+    let str = '';
+    if (!post.moderatorsIds?.length) {
+      str += 'none';
+    } else {
+      post.moderatorsIds?.map((m: User, i: number) => {
+        if (i === 0) {
+          str += m.username;
+          return;
+        }
+        if (post.moderatorsIds.length > 1 && i === post.moderatorsIds.length - 1) {
+          str += ' and ';
+          str += m.username;
+        } else if(post.moderatorsIds.length > 2) {
+          str += ', ';
+          str += m.username;
+        }
+      });
+    }
+    setModerators(str);
+  }
 
   return (
     <StyledPost>
@@ -23,10 +54,12 @@ function Post({ id, post }: Props) {
           justifyContent="center"
           alignItems="center">
             <Grid item xs>
-              <Avatar />
+              <StyledAvatar>
+                {post?.ownerId?.username.charAt(0).toUpperCase()}
+              </StyledAvatar>
             </Grid>
             <Grid item xs>
-              #username
+              {post.ownerId?.username}
             </Grid>
         </Grid>
         
@@ -37,21 +70,29 @@ function Post({ id, post }: Props) {
           spacing={2}
           >    
           <Grid item xs>
-            #title
+            {post.title}
           </Grid>
           <Grid item xs>
-            #content
+            {post.content}
           </Grid>
         </Grid>
-        <Grid item xs={2}>
+        <StyledLeftGrid item xs={2}>
           <EditIcon />
-        </Grid>
-        <Grid item xs={8}>
-          Post moderators: 
-        </Grid>
-        <Grid item xs={4}>
-          #posted_at
-        </Grid>
+        </StyledLeftGrid>
+
+        <StyledBottomGrid
+          item xs={12}
+          container
+          direction="row"
+          spacing={2}
+          >   
+          <Grid item xs={8}>
+            Post moderators: {moderators}
+          </Grid>
+          <StyledLeftGrid item xs={4}>
+            {date} {time.getHours()}:{time.getMinutes()}
+          </StyledLeftGrid>
+        </StyledBottomGrid>
         
       </StyledGrid> 
     </StyledPost>
