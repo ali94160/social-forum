89de689aa2aff10c9d185e4ae89e7431a0fd57db
autumnjava <creizy@gmail.com>
2022-01-18 14:48:38 +0100

    added delete comment by id

--- backend/api/comment/commentService.js
+++ backend/api/comment/commentService.js
@@ -1,1 +1,42 @@
-module.exports = function (app) {};
+const commentModel = require("../../models/comment");
+const { authUserLoggedIn, authRole } = require("../../middlewares/acl");
+const roles = require("../../models/role");
+
+module.exports = function (app) {
+    // ta bort sin kommentar
+    app.delete('/api/user/comments/:id',
+    authUserLoggedIn, authRole([roles.USER]),
+    async (req, res) => {
+        const user = { ...req.session.user };
+        commentModel.findOneAndDelete({ writeId: user._id, _id: req.params.id }, (err, comment) => {
+            if(err) {
+                res.sendStatus(403);
+                return;
+            }
+            res.status(200).json({message: 'Successfully deleted', comment});
+        });
+    })
+
+
+    // post comment for testing purposes
+    app.post(
+    "/api/comments",
+    authUserLoggedIn,
+    async (req, res) => {
+      try {
+        let comment = new commentModel({
+            content: 'test blabla ',
+            writeId: req.session.user._id,
+            createdDate: Date.now(),
+            postId: null
+        });
+        await comment.save();
+        res.sendStatus(200);
+        return;
+      } catch (error) {
+        res.sendStatus(400);
+        return;
+      }
+    }
+  );
+};
