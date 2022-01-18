51bd560f81f55638f047f7fad40bd0abde5b6df2
oscargregorius <oscar.gregorius@gmail.com>
2022-01-18 11:26:46 +0100

    add comment backend done ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

--- backend/api/comment/commentService.js
+++ backend/api/comment/commentService.js
@@ -1,1 +1,28 @@
-module.exports = function (app) {};
+const commentModel = require("../../models/comment");
+const { authUserLoggedIn } = require("../../middlewares/acl");
+
+module.exports = function (app) {
+  app.post("/api/comments", authUserLoggedIn, async (req, res) => {
+    if (!req.body) {
+      res.sendStatus(403);
+      return;
+    }
+    try {
+      let newComment = new commentModel({
+        ...req.body,
+        createdDate: Date.now(),
+        writeId: req.session.user._id,
+      });
+      const result = await newComment.save();
+      if (!result) {
+        res.sendStatus(400);
+        return;
+      }
+      res.status(200);
+      return;
+    } catch (error) {
+      res.sendStatus(400);
+      return;
+    }
+  });
+};
