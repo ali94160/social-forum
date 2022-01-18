be556b13077e9d0184d7d033568147445f40f939
oscargregorius <oscar.gregorius@gmail.com>
2022-01-18 11:21:08 +0100

    add comment backend done ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,12 +2,12 @@ const user = require("./user/userService");
 const auth = require("./auth/authService");
 const banlist = require("./ban/banService");
 const post = require("./post/postService");
-const comment = require("./commentService");
+const comment = require("./comment/commentService");
 
 module.exports = (app) => {
-user(app);
-auth(app);
-banlist(app);
-post(app);
-comment(app);
-}
+  user(app);
+  auth(app);
+  banlist(app);
+  post(app);
+  comment(app);
+};
