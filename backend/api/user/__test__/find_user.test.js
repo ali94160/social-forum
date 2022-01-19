67cc02e7f22a727666204570fbb89f4e9027835b
oscargregorius <oscar.gregorius@gmail.com>
2022-01-19 15:04:01 +0100

    test fix

--- backend/api/user/__test__/find_user.test.js
+++ backend/api/user/__test__/find_user.test.js
@@ -9,13 +9,13 @@ describe("Test if a user can can search for another user", () => {
   });
 
   test("user is not logged in", async () => {
-    const res = await testSession.get("/api/user/isabella");
+    const res = await testSession.get("/api/users/username/isabella");
     expect(res.statusCode).toBe(401);
   });
 
   test("user is logged in", async () => {
     await testSession.post("/api/login").send(testLogin2);
-    const res = await testSession.get("/api/user/isabella");
+    const res = await testSession.get("/api/users/username/isabella");
 
     expect(res.body.username).toBe("isabella");
     expect(res.body._id).toBeDefined();
@@ -23,7 +23,7 @@ describe("Test if a user can can search for another user", () => {
   });
 
   test("user is logged in and search for a user that does not exists", async () => {
-    const res = await testSession.get("/api/user/Isabella");
+    const res = await testSession.get("/api/users/username/Isabella");
 
     expect(res.body).toBe(null);
     expect(res.statusCode).toBe(200);
