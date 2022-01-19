52751f3348e3499145edab865c902658fd1d7582
Qiling <professorqtaku@gmail.com>
2022-01-19 14:53:47 +0100

    rearrange code

--- backend/api/comment/commentService.js
+++ backend/api/comment/commentService.js
@@ -1,5 +1,5 @@
 const Comment = require("../../models/comment");
-const Post = require("../../models/post")
+const Post = require("../../models/post");
 const { authUserLoggedIn, authRole } = require("../../middlewares/acl");
 const roles = require("../../models/role");
 
@@ -34,36 +34,35 @@ module.exports = function (app) {
     authRole([roles.ADMIN, roles.POSTOWNER, roles.POSTMODERATOR]),
     async (req, res) => {
       const user = { ...req.session.user };
-      const id = req.params.id;
-      try { 
-        const comment = await Comment.findOne({ _id: id }).exec();
+      try {
+        const comment = await Comment.findOne({ _id: req.params.id }).exec();
         const post = await Post.findOne({ _id: comment.postId }).exec();
-        // check if user is admin
-        if (user.roles.includes(roles.ADMIN)) {
-          // delete
-        }
         
-        // check if user is owner
-        if (user.roles.includes(roles.POSTOWNER)) {
-          const isOwner = post.ownerId.toString() === user._id
-          if (isOwner) {
-            // delete
-          }
-        }
+        let hasAccess = user.roles.includes(roles.ADMIN);
         
-        // check if user is moderator
-        if (user.roles.includes(roles.POSTMODERATOR) && post.moderatorIds.length > 0) {
-          const isModerator = post.moderatorsIds.includes(user._id)
-          if (isModerator) {
-            // delete
-          }
+        if (!hasAccess && user.roles.includes(roles.POSTOWNER)) {
+          hasAccess = post.ownerId.toString() === user._id;
         }
-      }
-      catch (error) {
-        res.sendStatus(400)
+
+        if (
+          !hasAccess &&
+          user.roles.includes(roles.POSTMODERATOR) &&
+          post.moderatorIds.length > 0
+        ) {
+          hasAccess = post.moderatorsIds.includes(user._id);
+        }
+
+        if (hasAccess) {
+          await Comment.findOneAndDelete({ _id: req.params.id });
+          res.sendStatus(200);
+        } else {
+          res.sendStatus(400);
+        }
+        return;
+      } catch (error) {
+        res.sendStatus(400);
         return;
       }
-      res.sendStatus(200)
     }
   );
 
