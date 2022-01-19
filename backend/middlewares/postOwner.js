9a46e0bd7dee2da00b68ce537d32d90b4009c5f2
postila <isabella.postila@hotmail.com>
2022-01-19 12:00:22 +0100

    created middleware
    so a post owner can update moderators list and a postmoderator can remove themself from the moderators list

--- backend/middlewares/postOwner.js
+++ backend/middlewares/postOwner.js
@@ -1,4 +1,5 @@
 const postModel = require("../models/post");
+const role = require("../models/role");
 
 async function isPostOwner(req, res, next) {
   try {
@@ -12,6 +13,37 @@ async function isPostOwner(req, res, next) {
   }
 }
 
+async function handleModerator(req, res, next) {
+  try {
+    const post = await postModel.findOne({ _id: req.params.id }).lean().exec();
+    const isModerator = post.moderatorsIds.some((id) => id == req.session.user._id)
+
+    if (post.ownerId._id != req.session.user._id && !isModerator) {
+      return res.sendStatus(403);
+    }
+
+    if (post.ownerId._id == req.session.user._id) {
+      await postModel.updateOne({ _id: req.params.id },
+        {
+          $set: {
+            moderatorsIds: req.body.moderatorsIds
+          }
+        })
+      return res.sendStatus(200);
+    }
+
+    if (isModerator) {
+      await postModel.updateOne({ _id: req.params.id }, { $pull: { moderatorsIds: req.session.user._id } });
+      return res.sendStatus(200);
+    }
+    
+    next()
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+}
+
 module.exports = {
-  isPostOwner
+  isPostOwner,
+  handleModerator
 }
