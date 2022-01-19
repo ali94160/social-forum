af396a6560d4026f47bac2908a74dbfd44d15ba0
bextex <r.sarvegard@gmail.com>
2022-01-19 08:28:05 +0100

    moved method to own file

---
+++ frontend/src/components/post/HandleModerators.ts
@@ -0,0 +1,25 @@
+import { User } from '../../interfaces/User';
+import { PostItem } from '../../interfaces/Post';
+
+export const formatModStr = (post: PostItem | undefined) => {
+  let str = '';
+    if (!post?.moderatorsIds?.length) {
+      str += 'none';
+    } else {
+      post?.moderatorsIds?.map((m: User, i: number) => {
+        if (i === 0) {
+          str += m.username;
+          return;
+        }
+        if (post?.moderatorsIds.length > 1 && i === post?.moderatorsIds.length - 1) {
+          str += ' and ';
+          str += m.username;
+        } else if(post?.moderatorsIds.length > 2) {
+          str += ', ';
+          str += m.username;
+        }
+      });
+    }
+  return str;
+}
+
