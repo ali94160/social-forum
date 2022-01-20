8e075308d93e4c5fa5394589f74d6a7716de90df
postila <isabella.postila@hotmail.com>
2022-01-20 20:59:47 +0100

    added mock data

--- backend/api/post/__test__/mock_data.js
+++ backend/api/post/__test__/mock_data.js
@@ -25,4 +25,16 @@ const postId = "61e51b44d7e4aef1f1fc1005"
 
 const wrongPostId = "62e51b44d7e4aef1f1fc1005"
 
-module.exports = { post, postId, user2Login, user1Login, updPost, wrongPostId };
+const moderatorsList1 = { moderatorsIds: ["61e5229bb2af1db642834c8d"]}
+const moderatorsList2 = { moderatorsIds: ["61e5229bb2af1db642834c8d", "61e16838095050c88bfc4961"] }
+
+module.exports = {
+  post,
+  postId,
+  user2Login,
+  user1Login,
+  updPost,
+  wrongPostId,
+  moderatorsList1,
+  moderatorsList2
+};
