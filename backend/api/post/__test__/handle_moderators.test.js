2936e3fc9dc2eb727a3d0b64de02435c1d0ef5ef
postila <isabella.postila@hotmail.com>
2022-01-20 21:11:24 +0100

    updated description of tests

--- backend/api/post/__test__/handle_moderators.test.js
+++ backend/api/post/__test__/handle_moderators.test.js
@@ -9,7 +9,7 @@ const postModel = require("../../../models/post");
 
 
 describe("Handle moderators", () => {
-  test("If user is not logged in", async () => {
+  test("That visitor can not reach endpoint when not logged in", async () => {
     const res = await request.put("/api/posts/" + postId + "/moderators");
     expect(res.statusCode).toBe(401);
   });
@@ -21,28 +21,28 @@ describe("Handle moderators", () => {
     testSession = session(app);
   });
 
-  test("When post id does not exist", async () => {
+  test("That post is not found when none existing post-id is sent", async () => {
     await testSession.post("/api/login").send(user1Login);
     const res = await testSession.put("/api/posts/" + wrongPostId + "/moderators");
     expect(res.statusCode).toBe(404);
   });
 
-  test("That two post-moderators is added by a post-owner", async () => {
+  test("That post-owner can add moderators", async () => {
     await testSession.post("/api/login").send(user1Login);
     newPost = await testSession.post("/api/user/posts").send(post);
-    const res = await testSession.put("/api/posts/" + newPost.body._id + "/moderators").send(moderatorsList1);
-    const result = await postModel.findOne({ _id: newPost.body._id }).lean().exec();
-    expect(res.statusCode).toBe(200);
-    expect(result.moderatorsIds).toHaveLength(1);
-  });
-
-  test("That post-owner can update moderators", async () => {
-    await testSession.post("/api/login").send(user1Login);
     const res = await testSession.put("/api/posts/" + newPost.body._id + "/moderators").send(moderatorsList2);
     const result = await postModel.findOne({ _id: newPost.body._id }).lean().exec();
     expect(res.statusCode).toBe(200);
     expect(result.moderatorsIds).toHaveLength(2);
-    // await postModel.deleteMany({ title: "postTestTitle" }).exec();
+  });
+  
+  test("That post-owner can remove moderators", async () => {
+    await testSession.post("/api/login").send(user1Login);
+    const res = await testSession.put("/api/posts/" + newPost.body._id + "/moderators").send(moderatorsList1);
+    const result = await postModel.findOne({ _id: newPost.body._id }).lean().exec();
+    expect(res.statusCode).toBe(200);
+    expect(result.moderatorsIds).toHaveLength(1);
+    await postModel.deleteMany({ title: "postTestTitle" }).exec();
   })
 
   afterAll(done => {  
