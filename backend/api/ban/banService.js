bb03d7e9bb9402d03be09fc7e03ad8aca0dacab2
bextex <r.sarvegard@gmail.com>
2022-01-21 14:43:51 +0100

    added endpoint to get banned documents

--- backend/api/ban/banService.js
+++ backend/api/ban/banService.js
@@ -1,7 +1,21 @@
+const { authUserLoggedIn, authRole } = require("../../middlewares/acl");
+const Ban = require('../../models/ban');
+const roles = require("../../models/role");
+
 module.exports = banlist = (app) => {
-  app.get("/api/bans", (req, res) => {
-    // get the ban model
-    // find all emails
-    //response, error code
+  app.get("/api/bans", authUserLoggedIn, authRole([roles.ADMIN]), async (req, res) => {
+    try {
+      const response = await Ban.find({}).sort({banDate: 'desc'}).exec();
+      return res.status(200).json(response);
+    } catch (error) {
+      res.status(404).json(error);
+    }
   });
+
+  //unban 
+  app.delete('/api/bans/:id', authUserLoggedIn, authRole([roles.ADMIN]), async (req, res) => {
+
+  });
+
+
 };
