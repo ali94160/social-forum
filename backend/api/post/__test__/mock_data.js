1c488d5be32dee2a1080d547f12bd9382fb854fc
postila <isabella.postila@hotmail.com>
2022-01-21 08:17:28 +0100

    added mock data

--- backend/api/post/__test__/mock_data.js
+++ backend/api/post/__test__/mock_data.js
@@ -10,31 +10,32 @@ const updPost = {
   categoryId: "61dc3a622f8ecad1bc1367b2",
 }
 
+const user1Login = {
+  email: "isabella@haha.se",
+  password: "123",
+};
+
 const user2Login = {
   email: "post-tester@haha.se",
   username: "post-tester",
   password: "123"
 }
 
-const user1Login = {
-  email: "isabella@haha.se",
-  password: "123",
-};
-
 const postId = "61e51b44d7e4aef1f1fc1005"
-
 const wrongPostId = "62e51b44d7e4aef1f1fc1005"
 
-const moderatorsList1 = { moderatorsIds: ["61e5229bb2af1db642834c8d"]}
-const moderatorsList2 = { moderatorsIds: ["61e5229bb2af1db642834c8d", "61e16838095050c88bfc4961"] }
+const moderatorsList0 = { moderatorsIds: [] }
+const moderatorsList1 = { moderatorsIds: ["61e5229bb2af1db642834c8d"] } // masons id
+const moderatorsList2 = { moderatorsIds: ["61e5229bb2af1db642834c8d", "61e16838095050c88bfc4961"] } // masons & quiling ids
 
 module.exports = {
   post,
-  postId,
-  user2Login,
-  user1Login,
   updPost,
+  user1Login,
+  user2Login,
+  postId,
   wrongPostId,
+  moderatorsList0,
   moderatorsList1,
   moderatorsList2
 };
