a8806ac28427bb16e91bc91466e7557832fd67b6
postila <isabella.postila@hotmail.com>
2022-01-24 15:16:26 +0100

    Added styling for expanded categories list

--- frontend/src/components/admin/categories/StyledCategoryList.ts
+++ frontend/src/components/admin/categories/StyledCategoryList.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 interface Props {
   isOpen?: boolean;
@@ -6,4 +8,26 @@ interface Props {
 
 export const StyledCategoryList = styled.div<Props>`
   background-color: ${(props) => (props.isOpen ? 'var(--dark-teal)' : "var(--dark-blue)")};
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px 16px;
+`;
+
+export const StyledCategoryTitle = styled.p`
+  font-size: 1rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+  padding-left: 10px;
+  margin: 0.2rem;
+`;
+
+export const StyledDeleteIcon = styled(DeleteIcon)`
+  padding-right: 10px;
+    :hover {
+      cursor: pointer;
+    }
 `;
