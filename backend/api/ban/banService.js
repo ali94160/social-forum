5f6434e6b9d06cef01d37dd38a62ca2556f01791
bextex <r.sarvegard@gmail.com>
2022-01-24 10:35:18 +0100

    created unban endpoint

--- backend/api/ban/banService.js
+++ backend/api/ban/banService.js
@@ -1,11 +1,12 @@
 const { authUserLoggedIn, authRole } = require("../../middlewares/acl");
 const Ban = require('../../models/ban');
 const roles = require("../../models/role");
+const { hashUtil } = require("../utils");
 
 module.exports = banlist = (app) => {
   app.get("/api/bans", authUserLoggedIn, authRole([roles.ADMIN]), async (req, res) => {
     try {
-      const response = await Ban.find({}).sort({banDate: 'desc'}).exec();
+      const response = await Ban.find({}).sort({ banDate: 'desc' }).exec();
       return res.status(200).json(response);
     } catch (error) {
       res.status(404).json(error);
@@ -14,8 +15,28 @@ module.exports = banlist = (app) => {
 
   //unban 
   app.delete('/api/bans/:id', authUserLoggedIn, authRole([roles.ADMIN]), async (req, res) => {
+    const user = { ...req.session.user };
+    const password = req.body.password;
+
+    if (!password) {
+      /// check this one
+      return res.sendSatus(400);
+    }
+
+    const hash = hashUtil(req.body?.password);
+
+    if (user.password !== hash) {
+      return res.sendStatus(403);
+    }
+
+    try {
+      console.log('everythings good')
+      await Ban.findOneAndDelete({ _id: req.params.id });
+      return res.sendStatus(200);
+    } catch (error) {
+      res.status(404).json(error);
+    }
 
   });
-
 
 };
