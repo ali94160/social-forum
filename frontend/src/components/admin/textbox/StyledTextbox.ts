cebd558d5a7773cbbce0ce28f4ac8e2eb3cfe941
oscargregorius <oscar.gregorius@gmail.com>
2022-01-24 15:36:06 +0100

    last changes ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

--- frontend/src/components/admin/textbox/StyledTextbox.ts
+++ frontend/src/components/admin/textbox/StyledTextbox.ts
@@ -4,6 +4,7 @@ export const StyledTitleTypography = styled.p`
   margin-top: 0;
   font-size: 1.8rem;
   font-weight: bold;
+  color: white;
 `;
 
 export const StyledWrapper = styled.div``;
@@ -11,8 +12,10 @@ export const StyledWrapper = styled.div``;
 export const StyledTitle = styled.p`
   font-size: 1.5rem;
   font-weight: bold;
+  color: white;
 `;
 
 export const StyledText = styled.p`
   font-size: 1.2rem;
+  color: white;
 `;
