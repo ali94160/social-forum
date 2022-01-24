0c71e9458fabb5667dbef1c4b0720a03c3087118
oscargregorius <oscar.gregorius@gmail.com>
2022-01-24 14:52:17 +0100

    admin panel done ^
    Co-authored-by: Ali94160 <ali.al-mousawi@hotmail.com>

--- frontend/src/components/admin/textbox/StyledTextbox.ts
+++ frontend/src/components/admin/textbox/StyledTextbox.ts
@@ -1,8 +1,18 @@
-import styled from 'styled-components';
-import { Typography } from "@mui/material";
+import styled from "styled-components";
 
-export const StyledTitleTypography = styled(Typography)`
-  font-weight: 600;
-  font-size: larger;
+export const StyledTitleTypography = styled.p`
+  margin-top: 0;
+  font-size: 1.8rem;
+  font-weight: bold;
 `;
 
+export const StyledWrapper = styled.div``;
+
+export const StyledTitle = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const StyledText = styled.p`
+  font-size: 1.2rem;
+`;
