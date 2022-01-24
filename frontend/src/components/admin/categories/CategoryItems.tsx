7e4277229c713847fc65b4b95cdc9f376c92518f
postila <isabella.postila@hotmail.com>
2022-01-24 11:02:30 +0100

    Import grid for layout and icons

--- frontend/src/components/admin/categories/CategoryItems.tsx
+++ frontend/src/components/admin/categories/CategoryItems.tsx
@@ -2,6 +2,9 @@
 import React, { useState } from "react";
 import { Category } from "../../../interfaces/Category";
 import { useCategory } from "../../../context/CategoryContext";
+import Grid from '@mui/material/Grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function CategoryItems() {
   const { categories } = useCategory();
@@ -9,6 +12,17 @@ function CategoryItems() {
   return (
     <div>
       {categories.map((category: Category) => <p key={category._id}>{category.title}</p>)}
+    <Grid container spacing={0}>
+      <Grid item xs={8}>
+        <p>Titel</p>
+      </Grid>
+      <Grid item xs={2}>
+        <DeleteIcon/>
+      </Grid>
+      <Grid item xs={2}>
+        <EditIcon />
+      </Grid>
+    </Grid>
     </div>
   )
 }
