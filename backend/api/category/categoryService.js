03e3b2e4e6f0811618b2285ddbb5ba05f523199d
autumnjava <creizy@gmail.com>
2022-01-24 09:56:14 +0100

    template

--- backend/api/category/categoryService.js
+++ backend/api/category/categoryService.js
@@ -25,5 +25,29 @@ module.exports = function (app) {
     }
   });
 
+  // 17.5 Skapa en route för att hämta alla kategorier, sortera bokstavsordning förutom Övrigt som ska ligga sist (4)
+  app.get("/api/categories", async (req, res) => {
+    let categories = [];
+    try {
+      let data = await postModel
+        .find({})
+        // .sort( )
+        .collation({ locale: "en" })
+        .lean()
+        .exec();
+
+      // add logic
+
+    } catch (e) {
+      res.sendStatus(400);
+      return;
+    }
+    if (categories.length > 0) {
+      res.status(200).json(categories);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+
   
 };
