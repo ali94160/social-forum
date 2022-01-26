b559994dda004c2ebd6b8edc1f6b3e2e19cd8876
Qiling <professorqtaku@gmail.com>
2022-01-26 13:14:04 +0100

    fix add category

--- frontend/src/components/admin/categories/CategoryList.tsx
+++ frontend/src/components/admin/categories/CategoryList.tsx
@@ -1,38 +1,44 @@
 import { useState, BaseSyntheticEvent } from "react";
 import { Category } from "../../../interfaces/Category";
-import Card from '@mui/material/Card';
-import CategoryItem from './CategoryItem';
+import Card from "@mui/material/Card";
+import CategoryItem from "./CategoryItem";
 import { useCategory } from "../../../context/CategoryContext";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import CloseIcon from '@mui/icons-material/Close';
-import { StyledAddWrapper, StyledHeader, StyledInputWrapper } from './StyledCategoryList';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  StyledAddWrapper,
+  StyledHeader,
+  StyledInputWrapper,
+} from "./StyledCategoryList";
 import BasicTextField from "../../basics/basic-text-field/BasicTextField";
 import { StyledTealButton } from "../../basics/StyledTealButton";
-import PasswordModal from '../password-validation/PasswordModal';
-
+import PasswordModal from "../password-validation/PasswordModal";
 
 function CategoriesList() {
   const [display, setDisplay] = useState<boolean>(false);
-  const [title, setTitle] = useState<string>('')
-  const [icon, setIcon] = useState<string>('')
+  const [title, setTitle] = useState<string>("");
+  const [icon, setIcon] = useState<string>("");
   const { categories, addCategory } = useCategory();
-  const [isAdd, setIsAdd] = useState(false);
-  const [password, setPassword] = useState('');
+  const [isAdd, setIsAdd] = useState<boolean>(false);
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [status, setStatus] = useState(0);
 
   const addNewCategory = async () => {
     const newCategory = {
       title,
-      icon: icon !== '' ? icon : "tag"
-    }
+      icon: icon !== "" ? icon : "tag",
+    };
     const res = await addCategory({ category: newCategory, password });
     setStatus(res);
     if (res === 200) {
-      setPassword('');
+      setTitle("");
+      setIcon("");
+      setIsAdd(!isAdd);
+      setPassword("");
       setDisplay(!display);
     }
-  }
+  };
 
   const renderAddCategory = () => (
     <StyledInputWrapper>
@@ -53,31 +59,35 @@ function CategoriesList() {
         handleChange={(ev: any) => setIcon(ev.target.value)}
       />
 
-      <StyledTealButton type="submit" variant="contained" onClick={() => setIsAdd(!isAdd)}>
+      <StyledTealButton
+        type="submit"
+        variant="contained"
+        onClick={() => setIsAdd(!isAdd)}
+      >
         Add
       </StyledTealButton>
     </StyledInputWrapper>
   );
 
   return (
-    <><Card>
-      <StyledAddWrapper>
-        {!display ?
-          (<AddCircleIcon onClick={() => setDisplay(!display)} />) :
-          (<CloseIcon onClick={() => setDisplay(!display)} />)
-        }
-        <StyledHeader >
-          Add new category
-        </StyledHeader>
-      </StyledAddWrapper>
+    <>
+      <Card>
+        <StyledAddWrapper>
+          {!display ? (
+            <AddCircleIcon onClick={() => setDisplay(!display)} />
+          ) : (
+            <CloseIcon onClick={() => setDisplay(!display)} />
+          )}
+          <StyledHeader>Add new category</StyledHeader>
+        </StyledAddWrapper>
 
-    {display && renderAddCategory()}
+        {display && renderAddCategory()}
 
-      {categories.map((category: Category, index: number) =>
-        <CategoryItem key={category._id} category={category} />
-      )}
-    </Card>
-    {isAdd &&
+        {categories.map((category: Category, index: number) => (
+          <CategoryItem key={category._id} category={category} />
+        ))}
+      </Card>
+      {isAdd && (
         <PasswordModal
           isOpen={isAdd}
           password={password}
@@ -90,8 +100,10 @@ function CategoriesList() {
           setShowPassword={setShowPassword}
         >
           <p>Do you want to add {title} as a category?</p>
-        </PasswordModal>}</>
-  )
+        </PasswordModal>
+      )}
+    </>
+  );
 }
 
 export default CategoriesList;
